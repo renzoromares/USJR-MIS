041c27765b1c48785b1afb297f864a7d3d66ccbc
Enzo <enzonarvasa@gmail.com>
2020-08-03 11:10:22 +0800

    Cash Advance done. Urls not completed

--- MIS_SYSTEM/RoomTransfer/urls.py
+++ MIS_SYSTEM/RoomTransfer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .import views
-from RoomTransfer.views import RoomTransfer
+from RoomTransfer.views import RoomTransfer, RoomTransferPermanent, RoomTransferTemporary
 
 urlpatterns = [
    path('roomtransfer',RoomTransfer, name = "roomtransfer"),
    path('roomtransfer/<id>', RoomTransfer , name = "roomtransfer"),
-
+   path('roomtransferpermanent/<id>', RoomTransferPermanent, name = "roomtransferpermanent"),
+   path('roomtransfertemporary/<id>', RoomTransferTemporary, name ="roomtransfertemporary")
 ]
