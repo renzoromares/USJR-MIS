2fb5248fa27275d822c55c87e3bb636a399d83c9
unknown <lopezrobertjoshua@gmail.com>
2020-08-16 14:18:15 +0800

    Fix View PAO and Riso. No 0001.py for migration ready

--- MIS_SYSTEM/ViewRequest/views.py
+++ MIS_SYSTEM/ViewRequest/views.py
@@ -30,8 +30,8 @@ def ViewRequestFac(request,id):
     return render(request,"ViewRequestsFac.html", {'data' : data, 'dataForm' : dataForm})
 
 def ViewRequestPao(request,id):
-    data = Employee.objects.filter(form__Type='Risograph',form__Status='Pending',risograph__Price = None).values('risograph__Risograph_ID','risograph__Department','risograph__Paper_Type','risograph__No_of_Copies','risograph__Size','risograph__File','form__Status','form__Date_Approved','form__Date_Requested','form__Form_ID')
-    num1= data.count()/2
+    dataEmployee = Department.objects.prefetch_related('Id_Number').get(Id_Number = id)
+    data = Employee.objects.filter(form__Type='Risograph',form__Status='Pending',risograph__Price = None).distinct('risograph__Risograph_ID').values('risograph__Risograph_ID','risograph__Department','risograph__Paper_Type','risograph__No_of_Copies','risograph__Size','risograph__File','form__Status','form__Date_Approved','form__Date_Requested','form__Form_ID')
     if request.method == "POST": 
         updateRiso=risograph.objects.get(Risograph_ID=request.POST["Riso_ID"])
         updateRiso.Price= request.POST["price"]
@@ -41,11 +41,11 @@ def ViewRequestPao(request,id):
         update.Date_Approved=datetime.today().strftime('%Y-%m-%d')
         update.Status='Approved'
         update.save(update_fields=['Date_Approved', 'Status'])
-    return render(request,"ViewRequestPAO.html", {'data' : data, 'num1': num1}) 
+    return render(request,"ViewRequestPAO.html", {'data' : data, 'dataEmployee': dataEmployee}) 
 
 
 def ViewRequestIMS(request,id):
     data = Department.objects.prefetch_related('Id_Number').get(Id_Number = id)
-    dataForm = Employee.objects.exclude(form__Status = 'Pending', form__Type='Risograph').values('First_Name','Last_Name','form__Type','form__Date_Requested','form__Date_Approved','form__Status')
-    return render(request,"ViewRequestsFac.html", {'data' : data, 'dataForm' : dataForm})
+    dataForm = Employee.objects.filter(form__Status='Approved', form__Type__in=('File Leave','Make-up Class','Request for Certificate','RoomTransfer(Permanent)','RoomTransfer(Temporary)','ScheduleTransfer(Temporary)','ScheduleTransfer(Permanent)')).values('First_Name','Last_Name','form__Type','form__Date_Requested','form__Date_Approved','form__Status')
+    return render(request,"ViewRequestIMS.html", {'data' : data, 'dataForm' : dataForm})
 
