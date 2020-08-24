ba386be8f88af4c232a9c30aded55198fdf2ea31
Ecclesty <felescusoj@gmail.com>
2020-08-24 22:08:47 +0800

    may progress

--- MIS_SYSTEM/ViewRequest/views.py
+++ MIS_SYSTEM/ViewRequest/views.py
@@ -22,15 +22,31 @@ def ViewRequestReads(request,id):
 def ViewRequestFac(request,id):
     data = Department.objects.prefetch_related('Id_Number').get(Id_Number = id)
     if data.Status_Dept=='Chairman':
-        dataForm = Employee.objects.filter(department__department = data.department, memo_routing__Date_Chairman_Approved = None, form__Form_ID__isnull = False).distinct('memo_routing__id').values('memo_routing__id','First_Name','Last_Name','memo_routing__Type_Request','memo_routing__Date_Faculty_Submitted','Id_Number')
+        dataForm = Employee.objects.filter(department__department = data.department, memo_routing__Date_Chairman_Approved = None, form__Form_ID__isnull = False).distinct('memo_routing__id').values(
+            'memo_routing__id','First_Name','Last_Name','memo_routing__Type_Request','memo_routing__Date_Faculty_Submitted','Id_Number',
+            'leave__Employee_Status','leave__Typeof_Leave','leave__Date_Start','leave__Date_End','leave__Period_Days','leave__Reason',
+            'room_transfer__Type','room_transfer__Date_Notify','room_transfer__Subject','room_transfer__Offer_Code','room_transfer__Time_Day','room_transfer__Room_From','room_transfer__Room_To','room_transfer__Date_Effective','room_transfer__Reason',
+            'schedule_transfer__Type','schedule_transfer__Date_Notify','schedule_transfer__Subject','schedule_transfer__Offer_Code','schedule_transfer__Time_Day','schedule_transfer__Schedule_From','schedule_transfer__Schedule_To','schedule_transfer__Reason',
+            'risograph__Date','risograph__Department','risograph__Paper_Type','risograph__No_of_Copies','risograph__Size','risograph__File',
+            'makeup_class__College','makeup_class__Reason','makeup_class__OfferCode','makeup_class__Date','makeup_class__Time','makeup_class__Room','makeup_class__Date_Of',
+            'cash_advance__Cash_Amount','cash_advance__Reason',
+            'certifacate__Certificate_To_Request','certifacate__Others_Certificate','certifacate__Reason')
     elif data.Status_Dept=='Faculty':
         dataForm = Employee.objects.filter(Id_Number = id, form__Form_ID__isnull = False).values('form__Type','form__Date_Requested','form__Date_Approved','form__Status')
     elif data.Status_Dept=='Dean':
-        dataForm = Employee.objects.filter(department__College = data.College, memo_routing__Date_Dean_Approved = None, memo_routing__Date_Chairman_Approved__isnull = False, form__Form_ID__isnull = False).distinct('memo_routing__id').values('memo_routing__id','First_Name','Last_Name','memo_routing__Type_Request','memo_routing__Date_Faculty_Submitted','Id_Number')
+        dataForm = Employee.objects.filter(department__College = data.College, memo_routing__Date_Dean_Approved = None, memo_routing__Date_Chairman_Approved__isnull = False, form__Form_ID__isnull = False).distinct('memo_routing__id').values(
+            'memo_routing__id','First_Name','Last_Name','memo_routing__Type_Request','memo_routing__Date_Faculty_Submitted','Id_Number',
+            'leave__Employee_Status','leave__Typeof_Leave','leave__Date_Start','leave__Date_End','leave__Period_Days','leave__Reason',
+            'room_transfer__Type','room_transfer__Date_Notify','room_transfer__Subject','room_transfer__Offer_Code','room_transfer__Time_Day','room_transfer__Room_From','room_transfer__Room_To','room_transfer__Date_Effective','room_transfer__Reason',
+            'schedule_transfer__Type','schedule_transfer__Date_Notify','schedule_transfer__Subject','schedule_transfer__Offer_Code','schedule_transfer__Time_Day','schedule_transfer__Schedule_From','schedule_transfer__Schedule_To','schedule_transfer__Reason',
+            'risograph__Date','risograph__Department','risograph__Paper_Type','risograph__No_of_Copies','risograph__Size','risograph__File',
+            'makeup_class__College','makeup_class__Reason','makeup_class__OfferCode','makeup_class__Date','makeup_class__Time','makeup_class__Room','makeup_class__Date_Of',
+            'cash_advance__Cash_Amount','cash_advance__Reason',
+            'certifacate__Certificate_To_Request','certifacate__Others_Certificate','certifacate__Reason')
     elif data.Status_Dept=='VP Academics':
         dataForm = Employee.objects.filter(memo_routing__Date_Chairman_Approved__isnull = False, memo_routing__Date_Dean_Approved__isnull = False, memo_routing__Date_VP_Acad_Approved = None, form__Form_ID__isnull = False, memo_routing__Type_Request__in = ('File Leave','Make-up Class','Request for Certificate','RoomTransfer(Permanent)','RoomTransfer(Temporary)','ScheduleTransfer(Temporary)','ScheduleTransfer(Permanent)','Cash Advance')).distinct('memo_routing__id').values('memo_routing__id','First_Name','Last_Name','memo_routing__Type_Request','memo_routing__Date_Faculty_Submitted')
     elif data.Status_Dept=='President':
-        dataForm = Employee.objects.filter(memo_routing__Date_Chairman_Approved__isnull = False, memo_routing__Date_Dean_Approved__isnull = False, memo_routing__Date_President_Approved = None, form__Form_ID__isnull = False, memo_routing__Type_Request = 'Cash Advance').distinct('memo_routing__id').values('memo_routing__id','First_Name','Last_Name','memo_routing__Type_Request','memo_routing__Date_Faculty_Submitted')
+        dataForm = Employee.objects.filter(memo_routing__Date_Chairman_Approved__isnull = False, memo_routing__Date_Dean_Approved__isnull = False, memo_routing__Date_VP_Acad_Approved__isnull = False, memo_routing__Date_President_Approved = None, form__Form_ID__isnull = False, memo_routing__Type_Request = 'Cash Advance').distinct('memo_routing__id').values('memo_routing__id','First_Name','Last_Name','memo_routing__Type_Request','memo_routing__Date_Faculty_Submitted')
     elif data.Status_Dept=='HR':
         dataForm = Employee.objects.filter(memo_routing__Date_Chairman_Approved__isnull = False, memo_routing__Date_Dean_Approved__isnull = False, memo_routing__Date_VP_Acad_Approved__isnull = False, memo_routing__Date_President_Approved__isnull = False, memo_routing__Date_HR_Approved = None, form__Form_ID__isnull = False, memo_routing__Type_Request__in = ('Cash Advance','File Leave')).distinct('memo_routing__id').values('memo_routing__id','First_Name','Last_Name','memo_routing__Type_Request','memo_routing__Date_Faculty_Submitted')
     elif data.Status_Dept=='PAO':
