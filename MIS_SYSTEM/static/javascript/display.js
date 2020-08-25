4cc612413eff3013e56e37e3382a3fb1967d7629
Enzo <enzonarvasa@gmail.com>
2020-08-25 08:24:43 +0800

    Gabei

--- MIS_SYSTEM/static/javascript/display.js
+++ MIS_SYSTEM/static/javascript/display.js
@@ -1,6 +1,15 @@
+var myID;
 function openForm(clicked_id){
+  
+    if (document.getElementById('mahal').value ==  clicked_id ){
     document.querySelector('.bg-modal').style.display = 'flex';
-    alert(clicked_id);
+    
+    }
+    //alert(clicked_id);
+    alert(document.getElementById('types').value);
+    
+    
+    
 }
 
 function closeForm(){
