633cb722e825d39eb771e93936f8f5bc874d77f7
renzo <enzonarvasa@gmail.com>
2020-08-25 12:08:51 +0800

    New Janics with weird btnDetails

--- MIS_SYSTEM/static/javascript/display.js
+++ MIS_SYSTEM/static/javascript/display.js
@@ -1,16 +1,10 @@
-var myID;
+/*var myID;
 function openForm(clicked_id){
   
     if (document.getElementById('mahal').value ==  clicked_id ){
-    document.querySelector('.bg-modal').style.display = 'flex';
-    
+        document.querySelector('.bg-modal').style.display = 'flex';
     }
-    //alert(clicked_id);
-    alert(document.getElementById('types').value);
-    
-    
-    
-}
+}*/
 
 function closeForm(){
     document.querySelector('.bg-modal').style.display = 'none';
